6439b67fe7d0a8843da65f95848601268a2d03a0
xoma_star <40947642+xoma-star@users.noreply.github.com>
2022-11-18 04:31:39 +0000

    regr eq task3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import {
     standardDeviations
 } from './tasks/1.js'
 import table from './tasks/2.js'
-import task3 from './tasks/3.js'
+import task3, {student3, AAPL_MSFT, AAPL_META, META_MSFT} from './tasks/3.js'
 
 
 console.log({mathExpects, dispersions, standardDeviations, corCoefs, student})
@@ -15,3 +15,12 @@ console.log("\n\ntask2")
 console.log(table.map(x => x.map(v => v.toFixed(2)).join("\t")).join("\n"))
 console.log("\n\ntask3")
 console.log(task3.map(x => x.map(v => v.toFixed(2)).join("\t")).join("\n"))
+console.log("\n\ntask3 student")
+console.log(student3.map(x => x.map(v => v.toFixed(2)).join("\t")).join("\n"))
+console.log("\n\ntask 3 self\n")
+console.log("AAPL MSFT")
+console.log(AAPL_MSFT.map(x => x.map(v => v.toFixed(2)).join("\t")).join("\n"))
+console.log("\nAAPL META")
+console.log(AAPL_META.map(x => x.map(v => v.toFixed(2)).join("\t")).join("\n"))
+console.log("\nMETA MSFT")
+console.log(META_MSFT.map(x => x.map(v => v.toFixed(2)).join("\t")).join("\n"))
--- tasks/3.js
+++ tasks/3.js
@@ -1,6 +1,8 @@
 import transpose from "../common/transpose.js"
 import normalize from "../common/normalize.js"
 import correlation from "../common/correlation.js"
+import mathExpect from "../common/mathExpect.js"
+import dispersion from "../common/dispersion.js"
 
 const AAPL = [150.17, 154.09, 162.51, 165.35, 172.10, 171.52, 163.62, 155.81, 157.37, 150.70, 150.43, 138.20, 140.09, 138.38, 147.27]
 const MSFT = [256.72, 260.36, 280.74, 282.91, 291.91, 286.15, 268.09, 256.06, 264.46, 244.74, 237.92, 232.90, 234.24, 228.56, 242.12]
@@ -11,5 +13,16 @@ const results = transpose([AAPL, MSFT, META])
 const U = normalize(results)
 
 const task3 = correlation(U)
+
+export const student3 = task3.map(v => v.map(x => Math.abs(x) * Math.sqrt(U.length - 2) / Math.sqrt(1 - Math.pow(x, 2))))
+
+const regr_self = (a, b) => a.map((x, i) => {
+  const c = mathExpect(a) - task3[0][1]  * Math.sqrt(dispersion(mathExpect(a), a)) / Math.sqrt(dispersion(mathExpect(b), b)) * (mathExpect(b) - b[i])
+  return [x, b[i], c, x - c]
+})
+
+export const AAPL_MSFT = regr_self(AAPL, MSFT)
+export const META_MSFT = regr_self(META, MSFT)
+export const AAPL_META = regr_self(AAPL, META)
 
 export default task3
